ef16256f356a09fee85eaf346312dd00bc1eca02
Ömer Sakmar <omersakmar@gmail.com>
2022-11-15 15:16:20 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [color, setColor] = useState("");
+  const [answers, setAnswers] = useState([]);
+  const [roundResult, setRoundResult] = useState(undefined);
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  const handleOptions = () => {
+    let randomOptions = Math.floor(Math.random() * 16777215).toString(16);
+    return `#${randomOptions}`;
+  };
+
+  const getShuffledArr = (arr) => {
+    const newArr = arr.slice();
+    for (let i = newArr.length - 1; i > 0; i--) {
+      const rand = Math.floor(Math.random() * (i + 1));
+      [newArr[i], newArr[rand]] = [newArr[rand], newArr[i]];
+    }
+    return newArr;
+  };
+
+  const playRound = () => {
+    const actualColor = handleOptions();
+    setColor(actualColor);
+    setAnswers([actualColor, handleOptions(), handleOptions()]);
+  };
+
+  useEffect(() => {
+    playRound();
+  }, [roundResult]);
+  console.log(getShuffledArr(answers));
+  console.log(color);
+  let shuffleAnswers = getShuffledArr(answers);
+
+  const handleUserAnswer = (answer) => {
+    if (answer === color) {
+      setRoundResult(true);
+      playRound();
+    } else {
+      setRoundResult(false);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="game-info">
+        <h2>What's this about?</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Each round, you get 3 random colors. Guess which one matches that of
+          the box.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="col">
+        <div className="guess-me" style={{ background: color }}></div>
+        {shuffleAnswers.map((answer, index) => (
+          <button key={index} onClick={() => handleUserAnswer(answer)}>
+            {answer}
+          </button>
+        ))}
+      </div>
+      {roundResult === false && <div className="wrong-answer">Wrong</div>}
+      {roundResult === true && <div className="correct-answer">Correct</div>}
     </div>
   );
 }
